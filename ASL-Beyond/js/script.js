0e8ec9c6e8229e9395202e8025949e65f909d182
Dr. Paul Kenneth Shreeman <pkshreeman@gmail.com>
2018-09-10 15:31:21 -0700

    Updated the autoplay issue
    The autoplay no longer work.  Now, I am implemeting hover trigger to play the video.  Let's see how well that works on mobile.

--- ASL-Beyond/js/script.js
+++ ASL-Beyond/js/script.js
@@ -1,4 +1,15 @@
-var video = document.querySelector('video');
-video.addEventListener('ended', function(){ 
+/*var video = document.querySelector('video');
+video.addEventListener('ended', function () {
   $(".intro-video").html('<img src="img/Abey1.jpg">')
 });
+*/
+
+var figure = $(".video").hover(hoverVideo, hideVideo);
+
+function hoverVideo(e) {
+  $('video', this).get(0).play();
+}
+
+function hideVideo(e) {
+  $('video', this).get(0).pause();
+}
